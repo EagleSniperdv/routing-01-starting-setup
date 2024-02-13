7839d325f43b8be519f06f52b90aa1160b2fc196
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-02-13 22:18:34 +0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation ></the-navigation>
   <main>
-    <component :is="activePage"></component>
+    <router-view></router-view>
   </main>
 </template>
 
 <script>
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
 import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
     TheNavigation,
-    TeamsList,
-    UsersList,
   },
   data() {
     return {
-      activePage: 'teams-list',
+      
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
         { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+        { id: 't4', name: 'Database', members: [ 'u6'] },
       ],
       users: [
         { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
@@ -30,6 +27,7 @@ export default {
         { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
         { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
         { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+        { id: 'u6', fullName: 'Naboth Nyagoko', role: 'Database' },
       ],
     };
   },
@@ -39,11 +37,7 @@ export default {
       users: this.users,
     };
   },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
-  },
+  
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
-const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/teams'
+        },
+        {
+            path:'/teams' ,
+            component: TeamsList
+        },
+        {
+            path:'/users' ,
+            component: UsersList
+        },
+        {
+            path:'/teams/:teamId',
+            component:TeamMembers
+        }
+    ]
+});
+
+router.beforeEach(function(to,from, next){
+    console.log('Global before each')
+    console.log(to, from);
+    next();
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
